3ffdd14e81af483530d8a2de349683783b2106f9
geonhwiii <g01063962671@gmail.com>
2019-11-02 15:32:54 +0900

    Add Section & Copyright

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
+import Section from './components/Section';
+import Copyright from './components/CopyRight';
 
 function App() {
   return (
     <div>
       <Header />
+      <Section />
+      <Copyright />
     </div>
   );
 }
