1f65b103e8a2d9fc6ef7779e65e3eac35bf8b0c3
geonhwiii <g01063962671@gmail.com>
2019-11-03 21:30:03 +0900

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import Header from './components/Header';
-import Section from './components/Section';
-import Copyright from './components/CopyRight';
+import Home from './pages/Home';
+import Detail from './pages/Detail';
 
 function App() {
   return (
     <div>
       <Header />
-      <Section />
-      <Copyright />
+      <Route exact path='/' component={Home} />
+      <Route path='/detail' component={Detail} />
     </div>
   );
 }
