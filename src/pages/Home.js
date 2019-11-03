94a10129e958387484c46264aa87596cceaab46f
geonhwiii <g01063962671@gmail.com>
2019-11-03 21:29:40 +0900

    Add HomePage

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Section from '../components/Section';
+import Copyright from '../components/CopyRight';
+
+const Home = () => {
+  return (
+    <div>
+      <Section />
+      <Copyright />
+    </div>
+  );
+};
+
+export default Home;
