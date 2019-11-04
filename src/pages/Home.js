351da85a8a2b88e86ba997bb7e0098f1d40f5a64
geonhwiii <g01063962671@gmail.com>
2019-11-04 13:10:05 +0900

    Fetch album data by axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Section from '../components/Section';
 import Copyright from '../components/CopyRight';
 
 const Home = () => {
+  const [albums, setAlbums] = useState([]);
+
+  // TODO: Life Cycle Method
+  useEffect(() => {
+    fetchAlbums();
+  }, []);
+
+  // TODO: Fetch Albums Data
+  const fetchAlbums = async () => {
+    try {
+      const response = await axios.get('http://13.125.107.126:8000/api/album');
+      setAlbums(response.data);
+      return;
+    } catch (err) {
+      console.log(err);
+      return;
+    }
+  };
+
+  // TODO: no fetch data
+  if (!albums.length) return <div>Loading...</div>;
+
   return (
     <div>
-      <Section />
+      <Section albums={albums} />
       <Copyright />
     </div>
   );
