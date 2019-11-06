55cd5767e4353a292e02a3383b97c53ec158f313
geonhwiii <g01063962671@gmail.com>
2019-11-06 19:52:31 +0900

    Add Player, Play Music whenever page move

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import Header from './components/Header';
 import { Home } from './pages';
 import { Detail } from './pages';
+import Player from './components/Player';
 
 function App() {
+  const [trackUrl, setTrackUrl] = useState('');
+  const [hidePlayer, setHidePlayer] = useState(true);
   return (
     <div>
       <Header />
       <Route exact path='/' component={Home} />
-      <Route path='/detail/:id' component={Detail} />
+      <Route
+        path='/detail/:id'
+        render={props => (
+          <Detail
+            {...props}
+            setTrackUrl={setTrackUrl}
+            setHidePlayer={setHidePlayer}
+          />
+        )}
+      />
+      <Player
+        url={trackUrl}
+        hidePlayer={hidePlayer}
+        setHidePlayer={setHidePlayer}
+      />
     </div>
   );
 }
